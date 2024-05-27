b6c81639cb68eee74665c0ae16630dad327a2d03
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-27 20:15:36 +0530

    fixed button

--- src/app/components/FullScreenStoryViewe.tsx
+++ src/app/components/FullScreenStoryViewe.tsx
@@ -75,7 +75,7 @@ const FullScreenStoryViewer: React.FC<FullScreenStoryViewerProps> = ({ stories, 
         </div>
       >
       </div>
-         <button className="absolute top-4 right-4 bg-white text-black px-4 py-2 rounded z-50" onClick={onClose}>
+         <button className="absolute mt-10 top-4 right-4 bg-white text-black px-4 py-2 rounded z-50" onClick={onClose}>
         Close
       </button>
      
